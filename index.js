ecad9efc0024be55fac9ecb29ccc5f9a6e1902d4
Tommy <tomstuart123@hotmail.co.uk>
2019-12-30 22:52:34 +0000

    with alex and gees feedback

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// TO DO
+    // FRONT END
+        // Pre-load
+            // Splash page - EdenCup - Love yourself and the planet
+        // HeaderSection
+            // Add nav bar - Either dropdown or sections
+        // URGENT About section
+            // Update Text
+        // URGENT Shop section
+            // update text
+            // Add menstrual cup definition
+        // URGENT BENEFITS / HOW TO / Contact
+            // update text
+        // Switch Toggle
+
+    // BACK END
+        // pay for shopify
+        // style page
+        // link it all together
+
 const portfolioApp = {};
 
 portfolioApp.pageLoad = function () {
@@ -14,6 +34,13 @@ portfolioApp.events = function () {
         $('.benefitSection__toggleSectionOne').toggleClass('visuallyHidden');
         $('.benefitSection__toggleSectionTwo').toggleClass('visuallyHidden');
     })
+    const $hamburger = $(".hamburger");
+    const $dropdown = $(".dropdown");
+    $hamburger.on("click", function (e) {
+        console.log('clicked')
+        $hamburger.toggleClass("is-active");
+        $dropdown.toggleClass('dropdown-start')
+    });
 
     // in portfolio section, toggle the developer portfolio for the product one on click
 }
